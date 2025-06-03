f1f9905a01a056777c813231aa301fa48bace10a
Wasay <Wasay@DESKTOP-8M7FRUN>
2025-06-03 20:01:19 +0500

    hello

---
+++ src/main/java/com/mycompany/sem_end_project/SEM_END_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sem_end_project;
+
+/**
+ *
+ * @author Wasay
+ */
+public class SEM_END_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
